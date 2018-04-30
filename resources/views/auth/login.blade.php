75c1701464183161df45e54f623598ab628c793d
Mandeep Singh <im@msingh.me>
2018-04-30 17:26:20 +0530

    navbar update & login page
    Signed-off-by: Mandeep Singh <im@msingh.me>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="flex items-center">
+<div class="flex items-center p-16 my-20">
     <div class="md:w-1/2 md:mx-auto">
         <div class="rounded shadow">
-            <div class="font-medium text-lg text-brand-darker bg-brand-lighter p-3 rounded-t">
+            <div class="font-medium text-lg text-brand-lightest bg-brand-darkest p-3 rounded-t">
                 Login
             </div>
             <div class="bg-white p-3 rounded-b">
--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="flex items-center justify-between flex-wrap bg-brand-darkest px-6 py-2">
+<nav class="flex items-center justify-between flex-wrap bg-brand-darkest px-16 py-4">
     <div class="flex items-center flex-no-shrink text-white mr-6">
         <a href="{{ route('home') }}" class="text-lg flex no-underline text-white">
             <img src="{{ asset('/img/logo.png') }}" width="80px" height="80px" class=""/>
@@ -19,8 +19,6 @@
     
     <div class="w-full block text-right flex-grow lg:flex lg:items-center lg:w-auto">
 
-        @if( !request()->route()->named('admin*') )
-
         <div class="text-sm lg:flex-grow">
             <a href="{{ route('home') }}" class="block mt-4 lg:inline-block lg:mt-0 text-white no-underline text-lg hover:text-brand-lighter mr-6">
                 Home
@@ -36,62 +34,53 @@
             </a>
 
             @guest
-                <a class="no-underline hover:underline text-white pr-3 text-sm" href="{{ url('/login') }}">Login</a>
-                <a class="no-underline hover:underline text-white text-sm" href="{{ url('/register') }}">Register</a>
+                <a class="block mt-4 lg:inline-block lg:mt-0 text-white no-underline text-lg hover:text-brand-lighter mr-6" href="{{ url('/login') }}">Login</a>
+                <!-- <a class="no-underline hover:underline text-white text-sm" href="{{ url('/register') }}">Register</a> -->
             @else
-            <a href="{{ route('admin.dashboard') }}" class="no-underline hover:underline text-white text-sm mr-3">
-                Dashboard
-            </a>
-            
-            <span class="text-grey-darker text-sm" style="float: right">Hi, {{ Auth::user()->name }}</span><br/>
-            <a href="{{ route('logout') }}" class="no-underline hover:underline text-white text-sm" onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">                
-                Logout
-            </a>
+                <a class="block mt-4 lg:inline-block lg:mt-0 text-white no-underline text-lg hover:text-brand-lighter mr-6" href="{{ route('admin.dashboard') }}">Dashboard</a>
             @endif
         </div>
-
-        @endif 
-
-        @if( !auth()->guest() && request()->route()->named('admin*') )
-
-        <div class="w-full block text-right flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow">
-            
-            @can('manage-users')
-                <a href="{{ route('admin.users.index') }}" class="block mt-4 lg:inline-block lg:mt-0 text-white no-underline text-lg hover:text-brand-lighter mr-6">
-                    Users
-                </a>
-            @endcan
-
-            @can('manage-equipments')
-            <a href="{{ route('admin.equipments.index') }}" class="block mt-4 lg:inline-block lg:mt-0 text-white no-underline text-lg hover:text-brand-lighter mr-6">
-                Equipments
-            </a>
-            @endcan
-
-            @can('manage-institutes')
-            <a href="{{ route('admin.institutes.index') }}" class="block mt-4 lg:inline-block lg:mt-0 text-white no-underline text-lg hover:text-brand-lighter mr-6">
-                Institutes
-            </a>
-            @endcan
-            
-            <a href="{{ route('home') }}" class="no-underline hover:underline text-white text-sm mr-3">
-                Website
-            </a>
-
-            <span class="text-grey-darker text-sm" style="float: right">Hi, {{ Auth::user()->name }}</span><br/>
-
-            <a href="{{ route('logout') }}" class="no-underline hover:underline text-white text-sm" onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">
-                Logout
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                {{ csrf_field() }}
-            </form>
-        </div>
-
-        @endif
-    </div>
     </div>
 </nav>
+
+@if( !auth()->guest() && request()->route()->named('admin*') )
+
+    <section class="py-4 px-16 text-center bg-grey-light">
+        <div class="">
+            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <p class="text-grey-darker text-sm float-left" >Hi, {{ Auth::user()->name }}</p>
+                <div class="text-sm lg:flex-grow text-right">
+    
+                    @can('manage-users')
+                        <a href="{{ route('admin.users.index') }}" class="block mt-4 lg:inline-block lg:mt-0 text-blue-dark no-underline text-lg hover:text-black mr-6">
+                            Users
+                        </a>
+                    @endcan
+
+                    @can('manage-equipments')
+                    <a href="{{ route('admin.equipments.index') }}" class="block mt-4 lg:inline-block lg:mt-0 text-blue-dark no-underline text-lg hover:text-black mr-6">
+                        Equipments
+                    </a>
+                    @endcan
+
+                    @can('manage-institutes')
+                    <a href="{{ route('admin.institutes.index') }}" class="block mt-4 lg:inline-block lg:mt-0 text-blue-dark no-underline text-lg hover:text-black mr-6">
+                        Institutes
+                    </a>
+                    @endcan
+    
+
+                    <a href="{{ route('logout') }}" class="block mt-4 lg:inline-block lg:mt-0 text-blue-dark no-underline text-lg hover:text-black mr-6" onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                        {{ csrf_field() }}
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+@endif
